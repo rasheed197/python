bad46026c0b2e9d2707627b270c7b7395168fd7b
rasheed197 <tiamiyurasheed.tr@gmail.com>
2025-08-20 08:43:02 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,33 @@
+# Python operators are special symbols used to perform specific operations on one or more operands.
+
+# The variables, values, or expressions can be used as operands.
+
+# Terms related to Python operators:
+
+# 1. Unary operators: Python operators that require one operand to perform a specific operation are known as unary operators.
+# 2. Binary operators: Python operators that require two operands to perform a specific operation are known as binary operators.
+# 3. Operands: Variables, values, or expressions that are used with the operator to perform a specific operation.
+
+# Types of Python Operators
+
+# 1. Arithmetic Operators 
+c = 10 + 20
+
+# 2. Comparison (Relational) Operators
+10 == 10
+
+# 3. Assignment Operators
+a = 10
+
+# 4. Logical Operators
+10 > 3 and 10 < 11
+
+# 5. Bitwise Operators
+c = 10 & 20
+
+# 6. Membership Operators
+print(2 in [1, 2, 3, 4])
+
+# 7. Identity Operators
+a=c=10
+print(a is c)
